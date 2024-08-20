c9b73503a9c2b4cd306e74c62f4a6ac94c31d89f
David Sherret <dsherret@users.noreply.github.com>
2024-08-20 17:56:49 -0400

    fix: deserialize v3 straight (#34)

--- src/error.rs
+++ src/error.rs
@@ -49,6 +49,8 @@ pub enum DeserializationError {
   InvalidNpmPackageDependency(String),
   #[error("Invalid package specifier '{0}'. Lockfile may be corrupt or you might be using an old version of Deno.")]
   InvalidPackageSpecifier(String),
+  #[error("Invalid jsr dependency '{dependency}' for '{package}'. Lockfile may be corrupt or you might be using an old version of Deno.")]
+  InvalidJsrDependency { package: String, dependency: String },
   #[error("npm package '{0}' was not found and could not have its version resolved. Lockfile may be corrupt.")]
   MissingPackage(String),
 }
